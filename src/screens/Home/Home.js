203bc0c7c4d8750ebfc2ffac894d84cf86156702
Salih Karaarslan <salihkaraarslan96@gmail.com>
2022-12-22 01:59:06 +0300

    style: change renderItem ui

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, FlatList, Text} from 'react-native';
+import {View, FlatList, Text, TouchableOpacity} from 'react-native';
 import styles from './Home.styles';
 
 import {useNavigation} from '@react-navigation/native';
@@ -11,12 +11,45 @@ const Home = () => {
 
   const {regions, routes} = Data;
 
-  const renderItem = ({item}) => <Text>{item.name}</Text>;
+  const renderItem = ({item}) => (
+    <TouchableOpacity
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: 'white',
+        marginVertical: 10,
+        height: 80,
+        paddingLeft: 30,
+        borderRadius: 10,
+        shadowColor: 'black',
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.15,
+        shadowRadius: 3,
+      }}
+      onPress={() => navigate('Detail', {region: item})}>
+      <Text
+        style={{
+          color: 'black',
+          fontSize: 20,
+          fontFamily: 'Poppins-SemiBold',
+        }}>
+        {item.name}
+      </Text>
+    </TouchableOpacity>
+  );
 
   return (
-    <View style={{flex: 1}}>
+    <View style={{flex: 1, backgroundColor: '#edf3fb'}}>
       <FlatList
-        contentContainerStyle={{flexGrow: 1, justifyContent: 'center'}}
+        contentContainerStyle={{
+          flexGrow: 1,
+          justifyContent: 'center',
+          marginHorizontal: 20,
+          marginBottom: 200,
+        }}
         scrollEnabled={false}
         data={regions}
         renderItem={renderItem}
