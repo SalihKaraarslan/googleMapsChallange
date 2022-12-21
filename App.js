f429aa04e046be152afce81b30928fef65c31905
Salih Karaarslan <salihkaraarslan96@gmail.com>
2022-12-22 01:12:41 +0300

    feat: add vector icons and fonts

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 import MapView from 'react-native-maps';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const App = () => {
   return (
     <View
       style={{
         flex: 1,
+        alignContent: 'center',
+        justifyContent: 'center',
       }}>
-      <MapView
+      <Text
+        style={{
+          color: 'black',
+          fontSize: 30,
+          fontFamily: 'Poppins-SemiBold',
+        }}>
+        asdasdasdasdsa
+      </Text>
+
+      {/* <MapView
         style={{
           flex: 1,
         }}
@@ -18,7 +30,9 @@ const App = () => {
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
-      />
+      /> */}
+      <Ionicons name="arrow-back" size={32} color={'black'} />
+      <Ionicons name="arrow-forward" size={32} color={'black'} />
     </View>
   );
 };
