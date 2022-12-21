c4625178567200fe4360503dc087aa997900028c
Salih Karaarslan <salihkaraarslan96@gmail.com>
2022-12-22 01:34:06 +0300

    feat: data listed

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,19 +1,27 @@
-import {View, Text, Button} from 'react-native';
 import React from 'react';
+import {View, FlatList, Text} from 'react-native';
+import styles from './Home.styles';
+
 import {useNavigation} from '@react-navigation/native';
+
+import Data from '../../mobileCaseData.json';
 
 const Home = () => {
   const {navigate} = useNavigation();
 
+  const {regions, routes} = Data;
+
+  const renderItem = ({item}) => <Text>{item.name}</Text>;
+
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}>
-      <Text>Home</Text>
-      <Button title="Go to Detail" onPress={() => navigate('Detail')} />
+    <View style={{flex: 1}}>
+      <FlatList
+        contentContainerStyle={{flexGrow: 1, justifyContent: 'center'}}
+        scrollEnabled={false}
+        data={regions}
+        renderItem={renderItem}
+        keyExtractor={item => item._id}
+      />
     </View>
   );
 };
